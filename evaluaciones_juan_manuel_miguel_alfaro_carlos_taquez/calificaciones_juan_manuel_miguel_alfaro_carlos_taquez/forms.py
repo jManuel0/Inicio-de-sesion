5f7462c0bd6b6f9a3d52cd8eb47cdf306e4cb721
miguelalfaro7 <miguel.alfaro@campusucc.edu.co>
2026-04-30 08:27:30 -0500

    avance frontend

--- evaluaciones_juan_manuel_miguel_alfaro_carlos_taquez/calificaciones_juan_manuel_miguel_alfaro_carlos_taquez/forms.py
+++ evaluaciones_juan_manuel_miguel_alfaro_carlos_taquez/calificaciones_juan_manuel_miguel_alfaro_carlos_taquez/forms.py
@@ -7,3 +7,38 @@ class CalificacionForm(forms.ModelForm):
     class Meta:
         model = Calificacion
         exclude = ('promedio',)
+        widgets = {
+            'nombre_estudiante': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Nombre completo',
+            }),
+            'identificacion': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Numero de identificacion',
+            }),
+            'asignatura': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Asignatura',
+            }),
+            'nota1': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': '0.00',
+                'step': '0.01',
+                'min': '0',
+                'max': '5',
+            }),
+            'nota2': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': '0.00',
+                'step': '0.01',
+                'min': '0',
+                'max': '5',
+            }),
+            'nota3': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': '0.00',
+                'step': '0.01',
+                'min': '0',
+                'max': '5',
+            }),
+        }
--- evaluaciones_juan_manuel_miguel_alfaro_carlos_taquez/calificaciones_juan_manuel_miguel_alfaro_carlos_taquez/views.py
+++ evaluaciones_juan_manuel_miguel_alfaro_carlos_taquez/calificaciones_juan_manuel_miguel_alfaro_carlos_taquez/views.py
@@ -1,7 +1,69 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.contrib import messages
+from django.db.models import Avg
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import CalificacionForm
+from .models import Calificacion
 
 
 @login_required
 def listar(request):
-    return HttpResponse("OK - listar (pendiente implementar plantilla/CRUD)")
+    calificaciones = Calificacion.objects.all().order_by('nombre_estudiante', 'asignatura')
+    promedio_general = calificaciones.aggregate(promedio=Avg('promedio'))['promedio']
+
+    return render(request, 'calificaciones/listar.html', {
+        'calificaciones': calificaciones,
+        'promedio_general': promedio_general,
+        'tiene_calificaciones': calificaciones.exists(),
+    })
+
+
+@login_required
+def crear(request):
+    if request.method == 'POST':
+        form = CalificacionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Calificacion creada correctamente.')
+            return redirect('calificaciones:listar')
+    else:
+        form = CalificacionForm()
+
+    return render(request, 'calificaciones/crear.html', {'form': form})
+
+
+@login_required
+def editar(request, pk):
+    calificacion = get_object_or_404(Calificacion, pk=pk)
+
+    if request.method == 'POST':
+        form = CalificacionForm(request.POST, instance=calificacion)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Calificacion actualizada correctamente.')
+            return redirect('calificaciones:listar')
+    else:
+        form = CalificacionForm(instance=calificacion)
+
+    return render(request, 'calificaciones/editar.html', {
+        'form': form,
+        'calificacion': calificacion,
+    })
+
+
+@login_required
+def eliminar(request, pk):
+    calificacion = get_object_or_404(Calificacion, pk=pk)
+
+    if request.method == 'POST':
+        calificacion.delete()
+        messages.success(request, 'Calificacion eliminada correctamente.')
+        return redirect('calificaciones:listar')
+
+    return render(request, 'calificaciones/eliminar.html', {'calificacion': calificacion})
+
+
+@login_required
+def promedio_general(request):
+    return listar(request)
